77bbb9cb04de036c47002d91722858e741b76f50
viren <viren.mandaogane14@gmail.com>
2020-08-24 09:21:25 +0530

    first commit

---
+++ test_viren/viren_test.py.py
@@ -0,0 +1,7 @@
+
+c=3*1e+08
+f=(80)
+x= 1e-06
+wavelength=(c/f)*x
+sentence = 'The wavelength for speed of light {}m/sec and frequency {}Mhz is {} in meters' . format(c,f,wavelength)
+print (sentence)
